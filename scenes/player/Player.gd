9ddbb85beeff54f4ef42dc699bae4e8b809e1acf
lpancho <leonard.pancho@gmail.com>
2019-08-26 22:42:30 +0800

    Added Wash skill

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,16 +1,19 @@
 extends KinematicBody2D
 
-
+onready var wash_scn = load("res://scenes/wash/Wash.tscn")
 onready var dash_skill_t = $DashSkillTimer
 onready var invi_skill_t = $InvisibleSkillTimer
+onready var wash_skill_t = $WashSkillTimer
 onready var invi_t = $InvisibleTimer
 onready var start_invi_t = $StartInvisibleTimer
 onready var dash_box = $CanvasLayer/HBoxContainer/DashBox
 onready var invi_box = $CanvasLayer/HBoxContainer/InvisibleBox
 onready var dash_progress = $CanvasLayer/HBoxContainer/DashBox/ProgressBar
 onready var invi_progress = $CanvasLayer/HBoxContainer/InvisibleBox/ProgressBar
+onready var wash_progress = $CanvasLayer/HBoxContainer/WashBox/ProgressBar
 onready var dash_prog_timer = $CanvasLayer/HBoxContainer/DashBox/DashProgTimer
 onready var invi_prog_timer = $CanvasLayer/HBoxContainer/InvisibleBox/InviProgTimer
+onready var wash_prog_timer = $CanvasLayer/HBoxContainer/WashBox/WashProgTimer
 onready var anim = $AnimModulate
 
 var SPEED = 350
@@ -30,7 +33,7 @@ func _physics_process(delta):
 	
 	var Q = Input.is_action_just_pressed("Q") # DASH
 	var W = Input.is_action_just_pressed("W") # INVISIBLE
-#	var E = Input.is_action_just_pressed("E") # WASH
+	var E = Input.is_action_just_pressed("E") # WASH
 	
 	if Q and dash_skill_t.is_stopped():
 		current_dash_value = DASH_VALUE
@@ -44,6 +47,11 @@ func _physics_process(delta):
 		invi_prog_timer.start()
 		$Invisible.play()
 		anim.play("Invisible")
+	if E and wash_skill_t.is_stopped():
+		var wash = wash_scn.instance()
+		wash.position = $Sprite.offset
+		add_child(wash)
+		wash_prog_timer.start()
 	target = get_global_mouse_position()
 	velocity = (target - position).normalized() * (SPEED + current_dash_value)
 	if (target - position).length() > 5:
@@ -75,6 +83,10 @@ func _on_InvisibleTimer_timeout():
 	set_collision_mask_bit(1, true)
 	pass # Replace with function body.
 
+func _on_WashSkillTimer_timeout():
+	print("wash skill stop")
+	pass # Replace with function body.
+
 func activate_skill(time):
 	var TIME_VALUE = 180
 	if (TIME_VALUE - time) % 45 == 0:
@@ -99,16 +111,27 @@ func _on_InviProgTimer_timeout():
 		invi_prog_timer.start()
 	pass # Replace with function body.
 
+func _on_WashProgTimer_timeout():
+	wash_progress.value -= 1
+	if wash_progress.value == 0:
+		wash_progress.value = wash_progress.max_value
+	else:
+		wash_prog_timer.start()
+	pass # Replace with function body.
+
 func stop_all():
 	set_physics_process(false)
 	dash_skill_t.stop()
 	invi_skill_t.stop()
+	wash_skill_t.stop()
 	start_invi_t.stop()
 	
 	dash_progress.value = dash_progress.max_value
 	dash_prog_timer.stop()
 	invi_progress.value = invi_progress.max_value
 	invi_prog_timer.stop()
+	wash_progress.value = wash_progress.max_value
+	wash_prog_timer.stop()
 
 func start_invi():
 	set_collision_mask_bit(1, false)
@@ -120,3 +143,6 @@ func _on_StartInvisibleTimer_timeout():
 	set_collision_mask_bit(1, true)
 	anim.play_backwards("Invisible")
 	pass # Replace with function body.
+
+
+
---
+++ scenes/wash/Wash.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+func _ready():
+	$Anim.play("default")
+	pass
+
+func _on_Anim_animation_finished(anim_name):
+	if anim_name == "default":
+		queue_free()
+	pass # Replace with function body.
